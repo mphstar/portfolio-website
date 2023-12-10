9f142923b3cc0910942a11c0815dff7f28574b19
mphstar <bintang.simdig20@gmail.com>
2023-12-10 17:44:03 +0700

    fix reserve

--- src/pages/portofolio.tsx
+++ src/pages/portofolio.tsx
@@ -6,9 +6,9 @@ import Image from "next/image";
 import CardPortfolio from "@/components/CardPortfolio";
 import data from "../models/data_portfolio.json";
 
-export default function portofolio() {
+const port = data.reverse();
 
-  const port = data.toReversed()
+export default function portofolio() {
   return (
     <Main selectedNav="Portfolio" title="Portfolio | @Mphstar">
       <div className="flex flex-col w-full flex-1 h-full mt-24 justify-start px-4 md:px-8 items-center overflow-hidden">
